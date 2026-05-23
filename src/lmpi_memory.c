f4521e41faad9e6e240755243c0dc6662b638b5d
Ehab Saleh <ehab.saleh@lrz.de>
2026-05-23 09:39:39 +0200

    Unified memory allocator for send and receive buffers

--- src/lmpi_memory.c
+++ src/lmpi_memory.c
@@ -3,42 +3,35 @@
 #include "internal/lmpi_memory_internal.h"
 #include "lmpi.h"
 
-MPI_Aint LMPI_Alloc(int count,MPI_Datatype datatype)
-{
-    int typesize;
-    MPI_Type_size(datatype,&typesize);
-    size_t size=count*typesize;
-    const size_t ALIGN=64;
-    shm_offset_recv=align_up(shm_offset_recv, ALIGN);
 
-    if (shm_offset_recv+size>LMPI_MAX_BUFFER) {
-        fprintf(stderr,"LMPI_Alloc: buffer overflow!\n");
-        MPI_Abort(MPI_COMM_WORLD, 1);
-    }
-
-    MPI_Aint allocated_offset=shm_offset_recv;
-    shm_offset_recv+=size;
-
-    return allocated_offset;
-}
-
-void* LMPI_Register(int count,MPI_Datatype datatype) {
+void* LMPI_Register(LMPI_PoolKind pool,int count,MPI_Datatype datatype) {
 
         int typesize;
         MPI_Type_size(datatype, &typesize);
         size_t size=count*typesize;
 
         const size_t ALIGN=64;
-        shm_offset_send=align_up(shm_offset_send, ALIGN);
-        if (shm_offset_send+size>LMPI_MAX_BUFFER) {
+        
+        if(pool==LMPI_POOL_SEND){
+            shm_offset_send=align_up(shm_offset_send, ALIGN);
+            if (shm_offset_send+size>LMPI_MAX_BUFFER){
                 fprintf(stderr,"LMPI_Alloc: buffer overflow!\n");
                 MPI_Abort(MPI_COMM_WORLD, 1);
-        }
+            }
 
-        void* ptr=(char*)shm_buf_send[local_rank]+shm_offset_send;
-        shm_offset_send+=size;
-        //MPI_Win_sync(shm_win_send);
-        return ptr;
+            void* ptr=(char*)shm_buf_send[local_rank]+shm_offset_send;
+            shm_offset_send+=size;
+        }
+        else if(pool==LMPI_POOL_RECV){
+            shm_offset_recv=align_up(shm_offset_recv, ALIGN);
+            if (shm_offset_recv+size>LMPI_MAX_BUFFER){
+                fprintf(stderr,"LMPI_Alloc: buffer overflow!\n");
+                MPI_Abort(MPI_COMM_WORLD, 1);    
+            }
+            void* ptr=(char*)shm_buf_recv[local_rank]+shm_offset_recv;
+            shm_offset_recv+=size;
+        }
+            return ptr;
 }
 
 
