135cab3b3af016fda17e6538ef6c19b9131b74a6
Ehab Saleh <ehab.saleh@lrz.de>
2026-05-23 10:55:56 +0200

    add LMPI_Free

--- src/lmpi_memory.c
+++ src/lmpi_memory.c
@@ -4,37 +4,69 @@
 #include "lmpi.h"
 
 
-void* LMPI_Malloc(LMPI_PoolKind pool,int count,MPI_Datatype datatype) {
+LMPI_Allocation LMPI_Malloc(LMPI_PoolKind pool,int count,MPI_Datatype datatype) {
+    int typesize;
+    MPI_Type_size(datatype, &typesize);
+    size_t size=count*typesize;
 
-        int typesize;
-        MPI_Type_size(datatype, &typesize);
-        size_t size=count*typesize;
-
-        const size_t ALIGN=64;
-        void *ptr=NULL;
-
-        if(pool==LMPI_POOL_SEND){
-            shm_offset_send=align_up(shm_offset_send, ALIGN);
-            if (shm_offset_send+size>LMPI_MAX_BUFFER){
-                fprintf(stderr,"LMPI_Alloc: buffer overflow!\n");
-                MPI_Abort(MPI_COMM_WORLD, 1);
-            }
-
-            ptr=(char*)shm_buf_send[local_rank]+shm_offset_send;
-            shm_offset_send+=size;
+    const size_t ALIGN=64;
+    LMPI_Allocation alloc;
+    alloc.ptr=NULL;
+    alloc.block.pool=pool;
+    alloc.block.size=size;
+    
+    if(pool==LMPI_POOL_SEND){
+        shm_offset_send=align_up(shm_offset_send, ALIGN);
+        if (shm_offset_send+size>LMPI_MAX_BUFFER){
+            fprintf(stderr,"LMPI_Alloc: buffer overflow!\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
         }
-        else if(pool==LMPI_POOL_RECV){
-            shm_offset_recv=align_up(shm_offset_recv, ALIGN);
-            if (shm_offset_recv+size>LMPI_MAX_BUFFER){
-                fprintf(stderr,"LMPI_Alloc: buffer overflow!\n");
-                MPI_Abort(MPI_COMM_WORLD, 1);    
-            }
-            ptr=(char*)shm_buf_recv[local_rank]+shm_offset_recv;
-            shm_offset_recv+=size;
+        alloc.ptr=(char*)shm_buf_send[local_rank]+shm_offset_send;
+        alloc.block.offset=shm_offset_send;
+        shm_offset_send+=size;
+    }
+    else if(pool==LMPI_POOL_RECV){
+        shm_offset_recv=align_up(shm_offset_recv, ALIGN);
+        if (shm_offset_recv+size>LMPI_MAX_BUFFER){
+            fprintf(stderr,"LMPI_Alloc: buffer overflow!\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);    
         }
+        alloc.ptr=(char*)shm_buf_recv[local_rank]+shm_offset_recv;
+        alloc.block.offset=shm_offset_recv;
+        shm_offset_recv+=size;
+    }
 
-        return ptr;
+    return alloc;
 }
 
 
+void LMPI_Free(LMPI_Allocation*alloc){
+    if(alloc==NULL||alloc->ptr==NULL){
+        return; 
+    }
+    LMPI_PoolKind pool=alloc->block.pool;
+    LMPI_Block *block=&alloc->block;
+    if(pool==LMPI_POOL_SEND){
+        if(block->offset+block->size!=shm_offset_send){
+            fprintf(stderr, "LMPI_Free: send pool free order violation\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        shm_offset_send=block->offset;
+    }
+    else if(pool==LMPI_POOL_RECV){
+        if(block->offset+block->size!=shm_offset_recv){
+               fprintf(stderr, "LMPI_Free: recv pool free order violation\n");
+               MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        shm_offset_recv=block->offset;
+        
+    }
+    else{
 
+        fprintf(stderr,"LMPI_Free:Invalid memory pool\n");
+         MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    alloc->ptr=NULL;
+    block->offset = 0;
+    block->size = 0;
+}
